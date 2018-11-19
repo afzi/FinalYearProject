1f50a3365c8a77291031f4faa85d1a45b7d18bc3
Kārlis Jaunslavietis <kjaunslavietis@gmail.com>
2018-11-19 14:58:01 +0000

    Created blank projects

---
+++ Source Code/Desktop/desktop-app/src/main/java/uk/ac/kent/co600/birdrings/desktopapp/DesktopAppApplication.java
@@ -0,0 +1,12 @@
+package uk.ac.kent.co600.birdrings.desktopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesktopAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesktopAppApplication.class, args);
+    }
+}
---
+++ Source Code/Desktop/desktop-ui/src/main/java/uk/ac/kent/co600/birdrings/desktopui/DesktopUiApplication.java
@@ -0,0 +1,12 @@
+package uk.ac.kent.co600.birdrings.desktopui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesktopUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesktopUiApplication.class, args);
+    }
+}
