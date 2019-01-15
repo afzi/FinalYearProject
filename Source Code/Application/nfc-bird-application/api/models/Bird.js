01640ae57e071401bee44085ca61925b3e050dc0
Priyesh Patel <prp6@kent.ac.uk>
2019-01-15 14:39:17 +0000

    All Model attributes completed

--- Source Code/Application/nfc-bird-application/api/models/Bird.js
+++ Source Code/Application/nfc-bird-application/api/models/Bird.js
@@ -6,35 +6,97 @@
 
 module.exports = {
     attributes: {
-        birdName: { type: 'string', required: true, unique: true, },
-        nfcRfid: { model: 'RFIDTag', unique: true, },
-        createdBy: { model: 'User', required: true, },
-        editeddBy: { model: 'User', required: true, },
-        studID: { type: 'number', },
-        newStudID: { type: 'number', },
-        leftRingID: { type: 'string', },
-        rightRingID: { type: 'string', },
-        sex: { type: 'string', },
-        motherName: { type: 'string', },
-        fatherName: { type: 'string' },
-        secondFatherName: { type: 'string', },
-        researcherNotes: { type: 'string', },
-        layDate: { type: 'ref', columnType: 'DATETIME', },
-        hatchDate: { type: 'ref', columnType: 'DATETIME', }, //if not set, Hatched = False
-        incubationDays: { type: 'number', },
-        whereHatched: { type: 'string', },
-        whereFledged: { type: 'string', },
-        whenFledged: { type: 'ref', columnType: 'DATETIME', }, //if not set, Fledged = false
-        whereReleased: { type: 'string', },
-        whenReleased: { type: 'ref', columnType: 'DATETIME', },
-        groupName: { type: 'string', },
-        currentNestSite: { type: 'string', },
+        birdID: {
+            type: 'number',
+            required: true,
+            unique: true,
+        },
+        birdName: {
+            type: 'string',
+            unique: true,
+        },
+        nfcRFID: {
+            model: 'RFIDTag',
+            unique: true,
+        },
+        createdBy: {
+            model: 'User',
+            required: true,
+        },
+        editedBy: {
+            model: 'User',
+            required: true,
+        },
+        studID: {
+            type: 'number',
+        },
+        newStudID: {
+            type: 'number',
+        },
+        leftRingID: {
+            type: 'string',
+        },
+        rightRingID: {
+            type: 'string',
+        },
+        sex: {
+            type: 'string',
+        },
+        motherName: {
+            type: 'string',
+        },
+        fatherName: {
+            type: 'string',
+        },
+        secondFatherName: {
+            type: 'string',
+        },
+        researcherNotes: {
+            type: 'string',
+        },
+        layDate: {
+            type: 'ref',
+            columnType: 'DATETIME',
+        },
+        hatchDate: { //if not set, Hatched = False
+            type: 'ref',
+            columnType: 'DATETIME',
+        },
+        incubationDays: {
+            type: 'number',
+        },
+        whereHatched: {
+            type: 'string',
+        },
+        whereFledged: {
+            type: 'string',
+        },
+        whenFledged: { //if not set, Fledged = false
+            type: 'ref',
+            columnType: 'DATETIME',
+        },
+        whereReleased: {
+            type: 'string',
+        },
+        whenReleased: {
+            type: 'ref',
+            columnType: 'DATETIME',
+        },
+        groupName: {
+            type: 'string',
+        },
+        currentNestSite: {
+            type: 'string',
+        },
+        dateOut: {
+            type: 'ref',
+            columnType: 'DATETIME',
+        },
         //GENERATED OTF -- could save computation time by storing one time calcs (hatched, fledged, seenSinceFlegded)
         //birdStatus
         //seenSinceFledged
         //recentlySeen
         //lastSeenDate
         //deathDate
-        dateOut: { type: 'ref', columnType: 'DATETIME', },
     },
 };
--- Source Code/Application/nfc-bird-application/api/models/RFIDTag.js
+++ Source Code/Application/nfc-bird-application/api/models/RFIDTag.js
@@ -4,4 +4,17 @@
  * link betwwen rfid tag name printed on ring and internal chip id
  */
 
-module.exports = {};
+module.exports = {
+    attributes: {
+        nfcRFID: {
+            type: 'number',
+            required: true,
+            unique: true,
+        },
+        nfcRFIDInternal: {
+            type: 'number',
+            required: true,
+            unique: true,
+        },
+    }
+};
