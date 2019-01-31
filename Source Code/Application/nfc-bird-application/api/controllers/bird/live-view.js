f391a4b793b8b9e4b0426c801e0b83fd0680442d
Priyesh Patel <prp6@kent.ac.uk>
2019-01-31 20:04:35 +0000

    dont make sense

--- Source Code/Application/nfc-bird-application/api/controllers/bird/live-view.js
+++ Source Code/Application/nfc-bird-application/api/controllers/bird/live-view.js
@@ -2,30 +2,32 @@ module.exports = {
 
 
     friendlyName: 'View live bird visits',
-  
-  
+
+
     description: 'gets information on birds that have recently visited',
-  
-  
+
+
     inputs: {
-  
+
     },
-  
-  
+
+
     exits: {
-  
-      success: {
-        description: 'Bird data '
-      },
-  
-      invalid: {
-        responseType: 'badRequest'
-      }
+
+        success: {
+            description: 'Bird data ',
+            viewTemplatePath: 'pages/bird/live-view'
+        },
+
+        invalid: {
+            responseType: 'badRequest'
+        }
     },
-  
-  
-    fn: async function (inputs) {
-      var LIVEVIEWQUERY = `
+
+
+    fn: async function() {
+        console.log("I MADE IT HERE 1");
+        var LIVEVIEWQUERY = `
       SELECT birds.id,birds.birdName, birds.leftRingID, birds.rightRingID, visits.createdAt 
       FROM nfcbirds.bird AS birds
       INNER JOIN nfcbirds.rfidtag AS tags
@@ -34,12 +36,11 @@ module.exports = {
       ON tags.nfcRFIDInternal = visits.ncfRFID
       WHERE visits.createdAt >= UNIX_TIMESTAMP(CURDATE());`;
 
-      var rawResult = await sails.sendNativeQuery(LIVEVIEWQUERY);
+        var rawResult = await sails.sendNativeQuery(LIVEVIEWQUERY);
 
-      console.log("I MADE IT HERE");
-      console.log(rawResult);
+        console.log("I MADE IT HERE 2");
+        console.log(rawResult);
 
-      return rawResult;
+        return exits.success(rawResult);
     }
-  };
-  
+};
