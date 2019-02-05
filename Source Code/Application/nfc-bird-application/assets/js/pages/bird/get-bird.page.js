b1367f72d7c02559f8a78a57073d8e246284966e
Afzi <aar23@kent.ac.uk>
2019-02-05 14:17:29 +0000

    birdss

---
+++ Source Code/Application/nfc-bird-application/assets/js/pages/bird/get-bird.page.js
@@ -0,0 +1,29 @@
+parasails.registerPage('get-bird', {
+    //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+    //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+    //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+    data: {
+        cloudSuccess: false,
+
+        result: {}
+    },
+
+    //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+    //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+    //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+    beforeMount: function() {
+        // Attach any initial data from the server.
+        _.extend(this, SAILS_LOCALS);
+    },
+    mounted: async function() {
+        this.result = await Cloud.getBird();
+        console.log("Got live view")
+    },
+
+    //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+    //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+    methods: {
+    
+    }
+});
