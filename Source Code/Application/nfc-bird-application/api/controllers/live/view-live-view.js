eb3ecf145ffdc0e9007fe8f0c80a6f6f20947952
Priyesh Patel <prp6@kent.ac.uk>
2019-02-05 14:00:34 +0000

    live view working-ish

--- Source Code/Application/nfc-bird-application/api/controllers/live/view-live-view.js
+++ Source Code/Application/nfc-bird-application/api/controllers/live/view-live-view.js
@@ -23,21 +23,23 @@ module.exports = {
 
     fn: async function(req, res) {
         sails.log("I MADE IT HERE 1");
-        var LIVEVIEWQUERY = `
-    SELECT birds.id,birds.birdName, birds.leftRingID, birds.rightRingID, visits.createdAt 
-    FROM nfcbirds.bird AS birds
-    INNER JOIN nfcbirds.rfidtag AS tags
-    ON birds.id = tags.birdID
-    INNER JOIN nfcbirds.visit AS visits
-    ON tags.nfcRFIDInternal = visits.ncfRFID
-    WHERE visits.createdAt >= UNIX_TIMESTAMP(CURDATE());`;
+        //     var LIVEVIEWQUERY = `
+        // SELECT birds.id,birds.birdName, birds.leftRingID, birds.rightRingID, visits.createdAt 
+        // FROM nfcbirds.bird AS birds
+        // INNER JOIN nfcbirds.rfidtag AS tags
+        // ON birds.id = tags.birdID
+        // INNER JOIN nfcbirds.visit AS visits
+        // ON tags.nfcRFIDInternal = visits.ncfRFID
+        // WHERE visits.createdAt >= UNIX_TIMESTAMP(CURDATE());`;
 
-        var rawResult = await sails.sendNativeQuery(LIVEVIEWQUERY);
+        //     var rawResult = await sails.sendNativeQuery(LIVEVIEWQUERY);
 
-        sails.log("I MADE IT HERE 2");
-        sails.log(rawResult);
+        //     sails.log("I MADE IT HERE 2");
+        //     sails.log(rawResult);
 
-        return rawResult;
+        //     return rawResult;
+
+        return {};
     }
 
 
