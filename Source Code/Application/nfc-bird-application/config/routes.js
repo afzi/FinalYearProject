c467c1105e6026dbd1fbe83454222919e051787c
Afzi <aar23@kent.ac.uk>
2019-02-14 13:13:09 +0000

    single bird view

--- Source Code/Application/nfc-bird-application/config/routes.js
+++ Source Code/Application/nfc-bird-application/config/routes.js
@@ -32,7 +32,7 @@ module.exports.routes = {
 
     'GET /birds': { action: 'bird/view-get-bird' },
 
-    'GET /birds/hello3': { action: 'bird/view-get-bird' },
+    // 'GET /birds/hello3': { action: 'bird/view-get-bird' },
 
     'GET /account/password': { action: 'account/view-edit-password' },
     'GET /account/profile': { action: 'account/view-edit-profile' },
