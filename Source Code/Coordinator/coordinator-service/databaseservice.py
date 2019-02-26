7e24eb77d4f61e724d7361d7cbec6c0762de1250
Kārlis Jaunslavietis <kjaunslavietis@gmail.com>
2019-02-26 20:00:46 +0000

    coordinator-service final

--- Source Code/Coordinator/coordinator-service/databaseservice.py
+++ Source Code/Coordinator/coordinator-service/databaseservice.py
@@ -1,54 +1,79 @@
 import mysql.connector
 import datetime
 import logging
+import sys
 
 class DatabaseService:
+    RFID_SHORT_COL_NAME = 'nfcRFID'
+    RFID_LONG_COL_NAME = 'nfcRFIDInternal'
+    COUNT1_COL_NAME = 'COUNT(1)'
+    BIRD_COL_NAME = 'birdID'
+    CREATEDAT_COL_NAME = 'createdAt'
+
     INSERT_QUERY = 'INSERT INTO visit(createdAt, birdID, nfcRFID) VALUES(%s,%s,%s)'
     CHECK_EXISTS_QUERY = 'SELECT COUNT(1) FROM visit WHERE createdAt > %s AND nfcRFID = %s'
     GET_RFID_QUERY = 'SELECT * FROM rfidtag WHERE nfcRFIDInternal = %s'
 
-    def __init__(self, config):
-        if config is not None and config.log_level is not None:
-            logging.basicConfig(level=config.log_level)
-        else: logging.basicConfig()
+    def __init__(self, params):
+        if params is not None and params.log_level is not None:
+            logging.basicConfig(level=params.log_level)
+        else:
+            logging.basicConfig()
 
-        self.db = mysql.connector.connect(
-            host=config.db_host,
-            user=config.db_user,
-            passwd=config.db_password,
-            port=config.db_port,
-            database=config.db_name,
-            autocommit=True,
-            auth_plugin= 'mysql_native_password'
-        )
+        try:
+            self.db = mysql.connector.connect(
+                host=params.db_host,
+                user=params.db_user,
+                passwd=params.db_password,
+                port=params.db_port,
+                database=params.db_name,
+                autocommit=True,
+                auth_plugin='mysql_native_password'
+            )
+        except (mysql.connector.errors.OperationalError, mysql.connector.errors.DatabaseError) as e:
+            logging.error("Couldn't connect to database, make sure it's running and the connection params are set in config.py")
+            raise Exception(e)
 
-        self.cutoff_minutes = config.same_visit_frequency_minutes
+        self.cutoff_minutes = params.same_visit_frequency_minutes
 
         logging.info("Connected to database successfully")
 
     def handle_new_visit(self, rfid, visit_datetime):
-        cursor = self.db.cursor(buffered=True, dictionary=True)
-        rfid_record = self.__get_rfid_record(rfid, cursor)
-        if rfid_record is None:
-            logging.debug("No RFID record found matching %s, ignoring this visit", rfid)
-        else:
-            logging.debug("Found RFID record with short ID %s matching long ID %s", rfid_record['nfcRFID'], rfid)
-            is_duplicate_visit = not self.__validate_not_duplicate_visit(cursor, rfid_record['nfcRFID'], visit_datetime - datetime.timedelta(minutes=self.cutoff_minutes))
-
-            if is_duplicate_visit:
-                logging.debug("Visit from RFID %s has already been recorded within the past %d minutes, so ignoring this visit", rfid, self.cutoff_minutes)
+        try:
+            cursor = self.db.cursor(buffered=True, dictionary=True)
+            rfid_record = self.__get_rfid_record(rfid, cursor)
+            if rfid_record is None:
+                logging.debug("No RFID record found matching %s, ignoring this visit", rfid)
             else:
-                logging.debug("Writing visit from %s at datetime %s to DB", rfid, visit_datetime.isoformat())
-                self.__write_visit(cursor, rfid_record['nfcRFID'], rfid_record['birdID'], visit_datetime)
+                logging.debug("Found RFID record with short ID %s matching long ID %s",
+                              rfid_record[self.RFID_SHORT_COL_NAME], rfid)
+                is_duplicate_visit = not self.__validate_not_duplicate_visit(cursor, rfid_record[self.RFID_SHORT_COL_NAME],
+                                                                             visit_datetime - datetime.timedelta(
+                                                                                 minutes=self.cutoff_minutes))
 
-        cursor.close()
+                if is_duplicate_visit:
+                    logging.debug(
+                        "Visit from RFID %s has already been recorded within the past %d minutes, so ignoring this visit",
+                        rfid, self.cutoff_minutes)
+                else:
+                    logging.debug("Writing visit from %s at datetime %s to DB", rfid, visit_datetime.isoformat())
+                    self.__write_visit(cursor, rfid_record[self.RFID_SHORT_COL_NAME], rfid_record[self.BIRD_COL_NAME],
+                                       visit_datetime)
+
+            cursor.close()
+        except (mysql.connector.errors.OperationalError, mysql.connector.errors.DatabaseError) as e: # something's gone wrong with the server, try to recover and if successful, retry this function
+            logging.warning("Disconnected from database, attempting to reconnect...")
+            print(e)
+            self.__reconnect()
+            self.handle_new_visit(rfid, visit_datetime)
+
 
     def __write_visit(self, cursor, rfid_short, bird_id, visit_datetime):
         cursor.execute(self.INSERT_QUERY, (visit_datetime.timestamp(), bird_id, rfid_short))
 
     def __validate_not_duplicate_visit(self, cursor, rfid_short, cutoff_time):
-        cursor.execute(self.CHECK_EXISTS_QUERY, (cutoff_time.timestamp(), rfid_short, ))
-        return cursor.fetchone()['COUNT(1)'] == 0
+        cursor.execute(self.CHECK_EXISTS_QUERY, (cutoff_time.timestamp(), rfid_short,))
+        return cursor.fetchone()[self.COUNT1_COL_NAME] == 0
 
     def __get_rfid_record(self, rfid, cursor):
         logging.debug("Fetching DB record for rfid %s", rfid)
@@ -56,16 +81,16 @@ class DatabaseService:
 
         if cursor.rowcount == 0:
             return None
-        else: return cursor.fetchone()
+        else:
+            return cursor.fetchone()
 
     def __reconnect(self):
-        logging.warning("Disconnected from Database, attempting to reconnect...")
+        logging.warning("Attempting to reconnect...")
         self.db.reconnect(attempts=5, delay=2)
         logging.info("Reconnect attempt successful")
 
     def __disconnect(self):
-        if(self.db is not None and self.db.is_connected()):
+        if (self.db is not None and self.db.is_connected()):
             self.db.disconnect()
             self.db.close()
             logging.info("Disconnected from database")
-
--- Source Code/Coordinator/coordinator-service/start.py
+++ Source Code/Coordinator/coordinator-service/start.py
@@ -2,29 +2,25 @@ import serial
 import sys
 import config
 import atexit
-import asyncio
 import logging
 import signal
 import databaseservice
 import datetime
+import threading
+import time
 
 class SerialReader:
-    def __init__(self, config): # TODO validation on config
-        if config is None or config.baud is None:
-            self.baud = 9600
-        else:
-            self.baud = config.baud
+    def __init__(self, params):
+        if not self.__validate_config_and_set_defaults(params):
+            raise Exception("Config validation failed, exiting")
 
-        if config is None or config.port is None:
-            self.port = 'COM5'  # if get time, write something that scans ports automatically
-        else:
-            self.port = config.port
+        self.baud = params.baud
 
-        if config is not None and config.log_level is not None:
-            logging.basicConfig(level=config.log_level)
-        else: logging.basicConfig()
+        self.port = params.port
 
-        self.db_service = databaseservice.DatabaseService(config)
+        logging.basicConfig(level=params.log_level)
+
+        self.db_service = databaseservice.DatabaseService(params)
 
         signal.signal(signal.SIGINT, self.signal_handler)
 
@@ -33,7 +29,7 @@ class SerialReader:
 
     def connect(self):
         try:
-            self.ser = serial.Serial(self.port, self.baud)
+            self.ser = serial.Serial(port=self.port, baudrate=self.baud, bytesize=serial.EIGHTBITS, stopbits=serial.STOPBITS_ONE)
             if not self.ser.is_open:
                 self.ser.open()
             atexit.register(self.__finished)
@@ -47,6 +43,61 @@ class SerialReader:
         logging.info("Connected to Serial Port %s, Name %s, Baud %s", self.port, self.ser.name, self.baud)
         self.is_connected = True
 
+    def __validate_config_and_set_defaults(self, config):
+        if config is None:
+            print.error("Missing config.py file")
+            return False
+        if config.db_host is None:
+            print("Missing required config param db_host")
+            return False
+
+        if config.db_port is None:
+            print("Missing required config param db_port")
+            return False
+
+        if config.db_user is None:
+            print("Missing required config param db_user")
+            return False
+
+        if config.db_password is None:
+            print("Missing required config param db_password")
+            return False
+
+        if config.db_name is None:
+            print("Missing required config param db_name")
+            return False
+
+        if config.log_level is None:
+            print("Mising optional config param log_level, using default = 'WARNING'")
+            config.log_level = 'WARNING'
+        elif config.log_level not in ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']:
+            print(
+                "Unknown log_level value %s in config, must be one of 'DEBUG' | 'INFO' | 'WARNING' | 'ERROR' | 'CRITICAL'. Using default = 'WARNING'",
+                config.log_level)
+            config.log_level = 'WARNING'
+
+        if config.baud is None:
+            print("Missing optional config param baud, using default = 9600")
+            config.baud = 9600
+        elif config.baud not in [50, 75, 110, 134, 150, 200, 300, 600, 1200, 1800, 2400, 4800, 9600, 19200, 38400,
+                                 57600, 115200]:
+            print(
+                "Unsupported baud rate %d in config, must be one of 50 | 75 | 110 | 134 | 150 | 200 | 300 | 600 | 1200 | 1800 | 2400 | 4800 | 9600 | 19200 | 38400 | 57600 | 115200. Using default = 9600",
+                config.baud)
+            config.baud = 9600
+
+        if config.same_visit_frequency_minutes is None:
+            print("Missing optional config param same_visit_frequency_minutes, using default = 60")
+            config.same_visit_frequency_minutes = 60
+        elif type(config.same_visit_frequency_minutes) is not int or config.same_visit_frequency_minutes < 0:
+            print("Invalid value %s used for config param same_visit_frequency_minutes, using default = 60")
+            config.same_visit_frequency_minutes = 60
+
+        if config.port is None:
+            print("Missing optional config param port, using default = 'COM5'")
+            config.baud = 'COM5'
+
+        return True
 
     def __validate_checksum(self, data, checksum):
         calculated_checksum = 0
@@ -56,30 +107,56 @@ class SerialReader:
         logging.debug("Calculated checksum = %d, received checksum = %d", calculated_checksum, checksum)
         return calculated_checksum == checksum
 
-    async def __handle_read(self, incoming_bytes):
-        visit_datetime = datetime.datetime.now()
-        # do some extra validation here, rather than just checksum (length, start / end bytes)
+    def __validate_packet(self, packet):
+        if not len(packet) == 13:
+            logging.debug("Incoming data has unexpected size. Expected 13 bytes, got %d", len(packet))
+            return False
+
+        start_byte = packet[0]
+        end_byte = packet[12]
+
+        if not (start_byte == 0xAA and end_byte == 0xBB):
+            logging.debug("Incoming data has wrong start byte or end byte. Expected 0xAA for start byte and 0xBB for end byte, got %X and %X, respectively", start_byte, end_byte)
+            return False
+
+        checksum = packet[11]
+        if not self.__validate_checksum(packet[1:11], checksum):
+            logging.debug("Incoming data checksum doesn't match")
+            return False
+
+        return True
+
+    def __handle_read(self, incoming_bytes, visit_datetime):
         logging.debug("Read raw data: %s", incoming_bytes.hex())
 
-        bird_tag_raw = incoming_bytes[4:11]
-        checksum = incoming_bytes[11]
-
-        logging.debug("Tag data: %s", bird_tag_raw.hex())
-        logging.debug("Checksum: %d", checksum)
-
-        if self.__validate_checksum(incoming_bytes[1:11], checksum):
-            logging.debug("Checksum matches, attempting to write data to DB")
-            self.db_service.handle_new_visit(bird_tag_raw.hex(), visit_datetime)
+        if self.__validate_packet(incoming_bytes):
+            logging.debug("Packet validated successfully, attempting to write data to DB")
+            bird_tag_raw = incoming_bytes[4:11].hex()
+            logging.debug("Tag data: %s", bird_tag_raw)
+            self.db_service.handle_new_visit(bird_tag_raw, visit_datetime)
         else:
-            logging.debug("Checksum doesn't match, discarding data")
+            logging.debug("Packet validation failed, discarding data")
 
     def listen(self):
         if self.is_connected in [None, False]:
             raise Exception("listen() called, but serial port not connected")
 
+        buffer = bytearray()
         while True:
-            read = self.ser.read(13) # try using readline here instead
-            asyncio.run(self.__handle_read(read))
+            read = self.ser.read(1)[0] # read one byte at a time
+
+            if read == 0xBB and len(buffer) >= 12:
+                # we've read the end byte and the buffer is at least as big as the expected packet size,
+                #  so construct a packet from what we have
+                buffer.append(read)
+                threading.Thread(target=self.__handle_read, args=(buffer,datetime.datetime.now())).start()
+                buffer = bytearray()
+            elif len(buffer) == 0 and read != 0xAA:
+                # the buffer is empty but we've read something other than the start byte, so let's discard this
+                logging.debug("Read byte %X but expecting start byte 0xAA, discarding this read", read)
+            else:
+                buffer.append(read)
+
 
     def __finished(self):
         if self.ser is not None and self.ser.is_open:
