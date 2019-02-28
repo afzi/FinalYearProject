0b471effcc367f92063b631bd5d307ba5baab382
Kārlis Jaunslavietis <kjaunslavietis@gmail.com>
2019-02-28 12:13:49 +0000

    Full name on user delete prompt

--- Source Code/Coordinator/coordinator-service/config.py
+++ Source Code/Coordinator/coordinator-service/config.py
@@ -1,6 +1,6 @@
 baud=9600 # default 9600
 port="COM5" # default COM5
-log_level="DEBUG" # DEBUG | INFO | WARNING | ERROR | CRITICAL
+log_level="DEBUG" # DEBUG | INFO | WARNING | ERROR | CRITICAL (default WARNING)
 
 db_host="localhost" # REQUIRED
 db_user="coordinator-root" # REQUIRED
@@ -8,4 +8,4 @@ db_password="7a&9WW$kASAsN-q7" # REQUIRED
 db_port=3306 # REQUIRED
 db_name="nfcbirds" # REQUIRED
 
-same_visit_frequency_minutes = 60 # REQUIRED
+same_visit_frequency_minutes = 60 # default 60
