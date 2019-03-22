2afd30b470d4cdf781d734db3328bdf9369f0f5f
Kārlis Jaunslavietis <kjaunslavietis@gmail.com>
2019-03-22 17:14:29 +0000

    User Activity Monitor only shows data diff + some bugfixes

--- Source Code/Application/nfc-bird-application/api/helpers/log-activity.js
+++ Source Code/Application/nfc-bird-application/api/helpers/log-activity.js
@@ -1,3 +1,24 @@
+let _ = require('underscore');
+
+function deepDiff(a, b, r, reversible) {
+  _.each(a, function(v, k) {
+    // already checked this or equal...
+    if (r.hasOwnProperty(k) || b && b[k] === v) return;
+    // but what if it returns an empty object? still attach?
+    r[k] = _.isObject(v) ? _.diff(v, b[k], reversible) : v;
+  });
+}
+
+/* the function */
+_.mixin({
+  diff: function(a, b, reversible) {
+    var r = {};
+    deepDiff(a, b, r, reversible);
+    if(reversible) deepDiff(b, a, r, reversible);
+    return r;
+  }
+});
+
 module.exports = {
 
     friendlyName: 'Log user activity',
@@ -36,11 +57,14 @@ module.exports = {
   
   
     fn: async function (inputs, exits) {
+      let newDataDiff = _.diff(inputs.newData, inputs.oldData);
+      let oldDataDiff = _.diff(inputs.oldData, inputs.newData);
+
       await Changelog.create({
           user: inputs.userId,
           action: inputs.action,
-          newData: JSON.stringify(inputs.newData, null, '\t'),
-          oldData: JSON.stringify(inputs.oldData, null, '\t')
+          newData: JSON.stringify(newDataDiff, null, '\t'),
+          oldData: JSON.stringify(oldDataDiff, null, '\t')
       })
       return exits.success();
     }
